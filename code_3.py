82382b2bd68053bb30f571e2d45f3c2e99d0567c
Polina <p9991820@gmail.com>
2023-10-06 06:18:57 +0300

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,53 +1,52 @@
+    data = [random.randint(1, 100) for _ in range(10)]
+import random
+def main():
 
 
         print(f"Random Number: {item}")
 
+
 def generate_random_data():
-import random
+    for item in data:
+    main()
+if __name__ == "__main__":
     data = generate_random_data()
-    data = [random.randint(1, 100) for _ in range(10)]
-    main()
-def main():
-
-    return data
-    for item in data:
-if __name__ == "__main__":
-        print(f"Random Number: {item}")
-if __name__ == "__main__":
+    return data    data = [random.randint(1, 100) for _ in range(10)]
 def generate_random_data():
-
-    for item in data:
     data = generate_random_data()
-    data = [random.randint(1, 100) for _ in range(10)]
-def main():
-import random
-
-    return data
-    main()
-
-import random
-    main()
-
-def main():
-        print(f"Random Number: {item}")
-
-def generate_random_data():
-    for item in data:
-    return data
-if __name__ == "__main__":
-
-    data = [random.randint(1, 100) for _ in range(10)]
-    data = generate_random_data()    data = generate_random_data()
-    data = [random.randint(1, 100) for _ in range(10)]
-
         print(f"Random Number: {item}")
 import random
-
 if __name__ == "__main__":
+
+
+
+    for item in data:
 def main():
+
+    return data
+    main()    data = [random.randint(1, 100) for _ in range(10)]
     return data
 
-def generate_random_data():
+def main():
+if __name__ == "__main__":
 
     main()
+import random
+
+        print(f"Random Number: {item}")
+    data = generate_random_data()
+
     for item in data:
+def generate_random_data():        print(f"Random Number: {item}")
+if __name__ == "__main__":
+    data = generate_random_data()
+
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+    for item in data:
+
+def generate_random_data():
+def main():
+
+    main()
