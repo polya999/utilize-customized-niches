df30e3c5e9df5668156a8b60a04d9df5c588a354
Polina <p9991820@gmail.com>
2023-10-08 18:43:43 +0300

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,52 +1,27 @@
+    for item in data:
+
     data = [random.randint(1, 100) for _ in range(10)]
-import random
 def main():
+
+    data = generate_random_data()
 
 
         print(f"Random Number: {item}")
-
-
-def generate_random_data():
-    for item in data:
     main()
-if __name__ == "__main__":
-    data = generate_random_data()
-    return data    data = [random.randint(1, 100) for _ in range(10)]
+    return data
 def generate_random_data():
-    data = generate_random_data()
-        print(f"Random Number: {item}")
 import random
 if __name__ == "__main__":
-
-
-
-    for item in data:
-def main():
-
-    return data
-    main()    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
     return data
 
-def main():
-if __name__ == "__main__":
-
-    main()
-import random
-
         print(f"Random Number: {item}")
-    data = generate_random_data()
-
-    for item in data:
-def generate_random_data():        print(f"Random Number: {item}")
 if __name__ == "__main__":
+    for item in data:
     data = generate_random_data()
-
 import random
+
     data = [random.randint(1, 100) for _ in range(10)]
-    return data
-    for item in data:
-
-def generate_random_data():
-def main():
-
     main()
+
+def main():
